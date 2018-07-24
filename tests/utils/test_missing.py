c06d6e6e006e8a3bb9df3e902e8020aa495cfe7c
Paul Stadnikov <paul.stadnikov@gmail.com>
2018-07-24 23:23:28 +0300

    test refactoring

--- tests/utils/test_missing.py
+++ tests/utils/test_missing.py
@@ -1,47 +1,59 @@
+from unittest import TestCase
+
 import numpy as np
 import pytest
 
 from dsmlt import utils
 
 
-def test_missing():
-    size = utils.random_size(3, low=1, high=10)
-    data_ndarray = utils.random_narray(size, p_missing=0.2)
-    result_ndarray = utils.missing(data_ndarray)
-    assert result_ndarray.dtype.type == np.bool_
-    assert result_ndarray.shape == size
+class TestMissingFunction(TestCase):
 
-    data_series = utils.random_series(20, p_missing=0.2)
-    result_series = utils.missing(data_series)
-    assert result_series.dtype.type == np.bool_
-    assert result_series.shape == (20, )
-    assert result_series.size == 20
+    def test_simple_numpy_ndarray(self):
+        size = utils.random_size(3, low=1, high=10)
+        data_ndarray = utils.random_narray(size, p_missing=0.2)
+        result_ndarray = utils.missing(data_ndarray)
+        assert result_ndarray.dtype.type == np.bool_
+        assert result_ndarray.shape == size
 
-    data_dataframe = utils.random_dataframe(3, 4, p_missing=0.2)
-    result_dataframe = utils.missing(data_dataframe)
-    for item_type in result_dataframe.dtypes.values:
-        assert item_type == np.bool_
-    assert result_dataframe.shape == (3, 4, )
-    assert result_dataframe.size == 12
+    def test_simple_pandas_series(self):
+        data_series = utils.random_series(20, p_missing=0.2)
+        result_series = utils.missing(data_series)
+        assert result_series.dtype.type == np.bool_
+        assert result_series.shape == (20,)
+        assert result_series.size == 20
 
-    with pytest.raises(AttributeError) as exc:
-        utils.missing('some wrong parameter here')
-    assert str(exc.value) == \
-        "Passed value `points` with invalid type - <class 'str'>."
+    def test_simple_pandas_dataframe(self):
+        data_dataframe = utils.random_dataframe(3, 4, p_missing=0.2)
+        result_dataframe = utils.missing(data_dataframe)
+        for item_type in result_dataframe.dtypes.values:
+            assert item_type == np.bool_
+        assert result_dataframe.shape == (3, 4,)
+        assert result_dataframe.size == 12
+
+    def test_wrong_data_type(self):
+        with pytest.raises(AttributeError) as exc:
+            utils.missing('some wrong parameter here')
+        assert str(exc.value) == \
+            "Passed value `points` with invalid type - <class 'str'>."
 
 
-def test_missing_count():
-    size = utils.random_size(3, low=1, high=10)
-    data_ndarray = utils.random_narray(size, p_missing=0.5)
-    assert utils.missing_count(data_ndarray) >= 1
+class TestMissingCountFunction(TestCase):
 
-    data_series = utils.random_series(20, p_missing=0.5)
-    assert utils.missing_count(data_series) >= 1
+    def test_simple_numpy_ndarray(self):
+        size = utils.random_size(3, low=1, high=10)
+        data_ndarray = utils.random_narray(size, p_missing=0.5)
+        assert utils.missing_count(data_ndarray) >= 1
 
-    data_dataframe = utils.random_dataframe(3, 4, p_missing=0.5)
-    assert utils.missing_count(data_dataframe) >= 1
+    def test_simple_pandas_series(self):
+        data_series = utils.random_series(20, p_missing=0.5)
+        assert utils.missing_count(data_series) >= 1
 
-    with pytest.raises(AttributeError) as exc:
-        utils.missing_count('some wrong parameter here')
-    assert str(exc.value) == \
-        "Passed value `points` with invalid type - <class 'str'>."
+    def test_simple_pandas_dataframe(self):
+        data_dataframe = utils.random_dataframe(3, 4, p_missing=0.5)
+        assert utils.missing_count(data_dataframe) >= 1
+
+    def test_wrong_data_type(self):
+        with pytest.raises(AttributeError) as exc:
+            utils.missing_count('some wrong parameter here')
+        assert str(exc.value) == \
+            "Passed value `points` with invalid type - <class 'str'>."
