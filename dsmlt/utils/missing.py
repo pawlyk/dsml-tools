d1f82c28f62ba97e2e6fd478b7b3fc368d5f0c0a
Paul Stadnikov <paul.stadnikov@gmail.com>
2018-08-03 16:40:06 +0300

    Feature/preprocessing improve missing (#8)
    * test refactoring
    
    * rid off unittest
    
    * add new test for optional parameter missing_value for missing function
    
    * improve missing function - add missing_value parameter, works with single value (int, float, string) and with list/tuple of parameters.
    
    * improve missing_count function - add missing_value parameter, works with single value (int, float, string) and with list/tuple of parameters. add tests  for it
    
    * little refactoring - move missing from utils/pandas to utils/ in case missing functions works not only with pandas data types
    
    * update docstrings

--- dsmlt/utils/missing.py
+++ dsmlt/utils/missing.py
@@ -9,33 +9,40 @@ __all__ = ('missing', 'missing_count', 'single_missing', )
 
 def single_missing(points,
                    single_missing_value: (int, float, str, None.__class__)):
-        """
-        Function that realize check on missing of given value.
+    """
+    Function that realize check on missing of given value.
 
-        Parameters:
-        -----------
-            points : numpy array, pandas Series, pandas DataFrame
-                An numobservations by numdimensions array of observations.
-            missing_value : int, float, str
-                A single value that we accept as indicator of missing value.
+    Parameters:
+    -----------
+        points : numpy array, pandas Series, pandas DataFrame
+            An numobservations by numdimensions array of observations.
+        missing_value : int, float, str
+            A single value that we accept as indicator of missing value.
 
-        Returns:
-        --------
-            mask : A numobservations-length boolean array.
-        """
-        if isinstance(points, (pd.DataFrame, pd.Series, np.ndarray)):
-            if single_missing_value is np.NAN or single_missing_value is None:
-                return pd.isnull(points)
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
 
-            else:
-                return points == single_missing_value
+    Raises:
+    -------
+        AttributeError
+            If passed invalid type of `points` value, e.g. all types
+            except pandas Series/DataFrame or numpy ndarray.
+            If passed invalid type of `missing_value` value.
+    """
+    if isinstance(points, (pd.DataFrame, pd.Series, np.ndarray)):
+        if single_missing_value is np.NAN or single_missing_value is None:
+            return pd.isnull(points)
 
         else:
-            raise AttributeError(
-                'Passed value `points` with invalid type - {}.'.format(
-                    type(points)
-                )
+            return points == single_missing_value
+
+    else:
+        raise AttributeError(
+            'Passed value `points` with invalid type - {}.'.format(
+                type(points)
             )
+        )
 
 
 def missing(points, missing_value: (int, float, str, list, tuple)=np.NAN):
@@ -54,6 +61,13 @@ def missing(points, missing_value: (int, float, str, list, tuple)=np.NAN):
     --------
         mask : numpy boolean array
             A numobservations-length boolean array.
+
+    Raises:
+    -------
+        AttributeError
+            If passed invalid type of `points` value, e.g. all types
+            except pandas Series/DataFrame or numpy ndarray.
+            If passed invalid type of `missing_value` value.
     """
     if isinstance(missing_value, (list, tuple)):
         result = single_missing(points, missing_value[0])
@@ -91,6 +105,13 @@ def missing_count(points,
     --------
         count : int
             A count of missing points.
+
+    Raises:
+    -------
+        AttributeError
+            If passed invalid type of `points` value        , e.g. all types
+            except pandas Series/DataFrame or numpy ndarray.
+            If passed invalid type of `missing_value` value.
     """
     if isinstance(points, (pd.DataFrame, pd.Series)):
         return missing(points, missing_value=missing_value).values.sum()
